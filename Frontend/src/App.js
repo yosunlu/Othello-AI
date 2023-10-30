a5d17a8c6bf99d979c609ffda9d4238185b32a91
yushanlu <yushanlu1031@gmail.com>
2023-10-30 01:09:25 -0500

    bug fixes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import "./GamePage.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import GamePage from "./GamePage";
 
 const App = () => {
--- Frontend/src/OthelloBoard.js
+++ Frontend/src/OthelloBoard.js
@@ -160,7 +160,7 @@ const OthelloBoard = (props) => {
       black++;
     }
 
-    onDataChange(white, black); // returns updated variables to parent
+    onDataChange(white, black);
   };
 
   return (
